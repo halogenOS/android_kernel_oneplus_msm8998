73f7a6a6a562933f8696ef2947b6300eb76358f3
Jaegeuk Kim <jaegeuk.kim@samsung.com>
2013-03-31 13:58:51 +0900

    f2fs: allocate remained free segments in the LFS mode
    This patch adds a new condition that allocates free segments in the current
    active section even if SSR is needed.
    Otherwise, f2fs cannot allocate remained free segments in the section since
    SSR finds dirty segments only.
    
    Reviewed-by: Namjae Jeon <namjae.jeon@samsung.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk.kim@samsung.com>
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -1737,6 +1737,17 @@ static int is_next_segment_free(struct f2fs_sb_info *sbi, int type)
 	return 0;
 }
 
+static int is_next_segment_free(struct f2fs_sb_info *sbi, int type)
+{
+	struct curseg_info *curseg = CURSEG_I(sbi, type);
+	unsigned int segno = curseg->segno;
+	struct free_segmap_info *free_i = FREE_I(sbi);
+
+	if (segno + 1 < TOTAL_SEGS(sbi) && (segno + 1) % sbi->segs_per_sec)
+		return !test_bit(segno + 1, free_i->free_segmap);
+	return 0;
+}
+
 /*
  * Find a new segment from the free segments bitmap to right order
  * This function should be returned with success, otherwise BUG
@@ -2009,6 +2020,8 @@ static void allocate_segment_by_default(struct f2fs_sb_info *sbi,
 		new_curseg(sbi, type, true);
 	else if (!is_set_ckpt_flags(sbi, CP_CRC_RECOVERY_FLAG) &&
 					type == CURSEG_WARM_NODE)
+		new_curseg(sbi, type, false);
+	else if (curseg->alloc_type == LFS && is_next_segment_free(sbi, type))
 		new_curseg(sbi, type, false);
 	else if (curseg->alloc_type == LFS && is_next_segment_free(sbi, type))
 		new_curseg(sbi, type, false);
