52354c467ac64d498c3ab882ae374a5aafd43bac
Pranav Vashi <neobuddy89@gmail.com>
2015-10-17 18:52:22 +0530

    mm: compaction: compact all zones when suspended
    * Less aggressive implementation of Motorola mod to
      compact all nodes when screen off.
    * Ref commit: https://github.com/MotorolaMobilityLLC/kernel-msm/commit/2101f55ccc48d097f5b98004ef8a18e2723b2163
    
    This is more of a workaround than a solution, but it will suffice for
    now to mitigate fragmentation to some extent.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -20,6 +20,9 @@
 #include <linux/kthread.h>
 #include <linux/freezer.h>
 #include <linux/page_owner.h>
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
+#endif
 #include "internal.h"
 
 #ifdef CONFIG_COMPACTION
@@ -1721,6 +1724,28 @@ break_loop:
 	return rc;
 }
 
+#ifdef CONFIG_STATE_NOTIFIER
+static void compact_nodes(void);
+
+static int state_notifier_callback(struct notifier_block *this,
+				unsigned long event, void *data)
+{
+	switch (event) {
+		case STATE_NOTIFIER_SUSPEND:
+			compact_nodes();
+			break;
+		default:
+			break;
+	}
+
+	return NOTIFY_OK;
+}
+
+static struct notifier_block compact_notifier_block = {
+	.notifier_call = state_notifier_callback,
+	.priority = -1,
+};
+#endif
 
 /* Compact all zones within a node */
 static void __compact_pgdat(pg_data_t *pgdat, struct compact_control *cc)
@@ -2070,4 +2095,12 @@ static int __init kcompactd_init(void)
 }
 subsys_initcall(kcompactd_init)
 
+#ifdef CONFIG_STATE_NOTIFIER
+static int  __init mem_compaction_init(void)
+{
+	state_register_client(&compact_notifier_block);
+	return 0;
+}
+late_initcall(mem_compaction_init);
+#endif
 #endif /* CONFIG_COMPACTION */
