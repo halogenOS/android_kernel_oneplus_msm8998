d5fbbc814314a3dff1a8ceafbcb8cf9bd353a6ef
Simão Gomes Viana <devel@superboring.dev>
2021-03-06 13:27:37 +0100

    Revert "fs: add useful debug info to eventpoll string"
    This reverts commit 1c13351d1c0ad1a866cb11b661ee19ac60e3a08a.

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -1236,11 +1236,8 @@ static int ep_create_wakeup_source(struct epitem *epi)
 	struct name_snapshot n;
 	struct wakeup_source *ws;
 
-	name = epi->ffd.file->f_path.dentry->d_name.name;
 	if (!epi->ep->ws) {
-		char buf[64];
-		snprintf(buf, sizeof(buf), "eventpoll pid:%d file:%s", current->pid, name);
-		epi->ep->ws = wakeup_source_register(buf);
+		epi->ep->ws = wakeup_source_register("eventpoll");
 		if (!epi->ep->ws)
 			return -ENOMEM;
 	}
